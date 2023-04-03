33ae48da1ad97020c742395d09a0350c50fe9fd2
87092rohitkumar <rohitkrltr121@gmail.com>
2023-04-03 11:53:54 +0530

    RotateArray90Degree

---
+++ Rotate2DArray90Degree1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    int mat[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> mat[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            swap(mat[i][j], mat[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n / 2; j++)
+        {
+            swap(mat[i][j], mat[i][n - j - 1]);
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << mat[i][j];
+        }
+
+        cout << mat[i][j<<" "];
+    }
+}
